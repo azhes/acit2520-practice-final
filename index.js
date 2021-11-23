01f100f8e7872a240eec98456fd2b40bd713702e
Azhes <graystonea@gmail.com>
2021-11-23 14:55:41 -0800

    finishes first question

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 8007;
 const app = express();
+const fs = require('fs').promises
 
 // Don't worry about these 4 lines below
 app.set("view engine", "ejs");
@@ -9,8 +10,26 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("homepage");
+  res.render("createcard");
 });
+
+app.post("/", async (req, res) => {
+  let formData = req.body;
+  let formName = req.body.name
+  let aboutMe = req.body.aboutme
+  let books = req.body.books
+  let artists = req.body.artists
+
+  console.log(formData)
+  const database = await fs.readFile('database.json', "utf8");
+  const obj = JSON.parse(database)
+  console.log(obj)
+  obj.users.push(formData)
+  await fs.writeFile('database.json', JSON.stringify(obj))
+
+  res.render("homepage", { id: Math.floor(Math.random() * 9999999), name: formName, aboutme: aboutMe, books: books, artists: artists })
+})
+
 app.get("/people/:id", (req, res) => {
   res.render("people");
 });
